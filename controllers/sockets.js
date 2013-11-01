3a4adc23841fe25fc22171b5e4a8576b1137c7e6
Josh Cai <josh.cai@yahoo.com>
2013-11-01 00:59:23 -0500

    minor changes to sockets

--- controllers/sockets.js
+++ controllers/sockets.js
@@ -8,7 +8,7 @@ module.exports = function(io){
 	io.sockets.on('connection', function (socket) {
 
 		socket.emit('news', { hello: 'world' });
-		socket.on('my other event', function (data) {
+		socket.on('answer', function (data) {
 			console.log(data);
 		});
 		
